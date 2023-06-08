3a7bba29ebcb34fe99fc7b3265db04e8869cbd3e
Ettie Fishman <123407548+ettiehi@users.noreply.github.com>
2023-06-08 11:48:25 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+//1.
+
+// const str =
+
+// `The Naming of Cats is a difficult matter,
+
+// It isn't just one of your holiday games;
+
+// You may think at first I'm as mad as a hatter
+
+// When I tell you, a cat must have THREE DIFFERENT NAMES.
+
+// First of all, there's the name that the family use daily,
+
+// Such as Peter, Augustus, Alonzo or James,
+
+// Such as Victor or Jonathan, George or Bill Bailey—
+
+// All of them sensible everyday names.
+
+// There are fancier names if you think they sound sweeter,
+
+// Some for the gentlemen, some for the dames:
+
+// Such as Plato, Admetus, Electra, Demeter—
+
+// But all of them sensible everyday names.
+
+// But I tell you, a cat needs a name that's particular,
+
+// A name that's peculiar, and more dignified,
+
+// Else how can he keep up his tail perpendicular,
+
+// Or spread out his whiskers, or cherish his pride?`;
+
+// const regex =/[^\s]+$/gm;
+
+// console.log(str.match(regex));
+
+//2.
+
+// const regex = /^[a-z0-9_-]{3,16}$/
+
+// const userName = 'ettiefi_11'
+
+// console.log(regex.test(userName))
+
+// const invalidUserName = 'username$%^'
+
+// console.log(regex.test(invalidUserName))
+
+//3.
+
+// const regex = /^[aeiouAEIOU]+$/;
+
+// console.log(regex.test('aui'))
